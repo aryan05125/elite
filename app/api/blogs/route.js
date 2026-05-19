dd0e79476f89ea0c5236b85194ba352190789c1c
aryan05125 <aryanchauhan144512@gmail.com>
2026-05-19 16:12:33 +0530

    remove mongodb fully

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -1,38 +1,10 @@
-// import connectDB from "@/lib/mongodb";
-import Blog from "@/models/Blog";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    // await connectDB();
-
-    const blogs = await Blog.find().sort({
-      createdAt: -1,
-    });
-
-    return Response.json(blogs);
-
-  } catch (error) {
-    return Response.json(
-      { message: "Error" },
-      { status: 500 }
-    );
-  }
-}
-
-export async function POST(req) {
-  try {
-    // await connectDB();
-
-    const body = await req.json();
-
-    const blog = await Blog.create(body);
-
-    return Response.json(blog);
-
-  } catch (error) {
-    return Response.json(
-      { message: "Error" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json([
+    {
+      title: "AI Automation",
+      description: "Powerful AI workflow automation.",
+    },
+  ]);
 }
